7dcb818d3cb000fcd564094bda6ec806f2d86bb8
Meeshanthini Dogan <mvdogan@users.noreply.github.com>
2015-12-06 22:21:36 -0600

    Update kNNskl.py

--- kNNskl.py
+++ kNNskl.py
@@ -87,32 +87,42 @@ def pixelArray (originalPath, skinPath, imgNames):
     return pixels
     
 
-originalTrainPath = "/Users/mvijayen/bda_project/Original/train/"
-skinTrainPath = "/Users/mvijayen/bda_project/Skin/train/"
+originalTrainPath = "/Shared/bdagroup5/Original/train/"
+skinTrainPath = "/Shared/bdagroup5/Skin/train/"
 imgTrainNames = [f for f in os.listdir(originalTrainPath) if not f.startswith('.')]
-img_train = pixelArray(originalTrainPath, skinTrainPath, imgTrainNames) 
-
-
-originalValPath = "/Users/mvijayen/bda_project/Original/val/"
-skinValPath = "/Users/mvijayen/bda_project/Skin/val/"
-imgValNames = [f for f in os.listdir(originalValPath) if not f.startswith('.')]
-img_val = pixelArray(originalValPath, skinValPath, imgValNames)
-
-
+img_train = pixelArray(originalTrainPath, skinTrainPath, imgTrainNames)
+train_shape = img_train.shape
 X_train = img_train[:,0:-1]
 Y_train = img_train[:,-1]
-X_val = img_val[:,0:-1]
-Y_val = img_val[:,-1]
-knn_start = time.time()
+knn_fit_start = time.time()
 knn = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='brute', metric='minkowski', p=2, metric_params=None)    
 knn.fit(X_train,Y_train)
-predictYval = knn.predict(X_val)
-knn_end = time.time()
-knn_time = knn_end - knn_start
-percent_accuracy = sklearn.metrics.accuracy_score(Y_val, predictYval, normalize=True, sample_weight=None)*100
-cm = sklearn.metrics.confusion_matrix(Y_val, predictYval)
-cm_vals = np.concatenate(([cm[0][0]], [cm[0][1]], [cm[1][0]], [cm[1][1]], [percent_accuracy], [knn_time]))
-np.savetxt("/Users/mvijayen/bda_project/processed/k1bs7.txt",cm_vals[None],fmt='%d, %d %d %d %f %f')
+knn_fit_end = time.time()
+fit_time = knn_fit_end - knn_fit_start
+print train_shape
+print "X and Y train complete"
+
+originalValPath = "/Shared/bdagroup5/Original/val/"
+skinValPath = "/Shared/bdagroup5/Skin/val/"
+imgValNames = [f for f in os.listdir(originalValPath) if not f.startswith('.')]
+imgValNames = imgValNames[0:2]
+print imgValNames
+
+with open("/Shared/bdagroup5/kNNprocessed/k1bs7.txt",'a') as fn:
+    for i in imgValNames:
+        print i
+        img_val = pixelArray(originalValPath, skinValPath, i)
+        X_val = img_val[:,0:-1]
+        Y_val = img_val[:,-1]
+        knn_predict_start = time.time()
+        predictYval = knn.predict(X_val)
+        knn_predict_end = time.time()
+        predict_time = knn_predict_end - knn_predict_start
+        knn_time = fit_time + predict_time
+        percent_accuracy = sklearn.metrics.accuracy_score(Y_val, predictYval, normalize=True, sample_weight=None)*100
+        cm = sklearn.metrics.confusion_matrix(Y_val, predictYval)
+        cm_vals = np.concatenate(([i], [cm[0][0]], [cm[0][1]], [cm[1][0]], [cm[1][1]], [percent_accuracy], [knn_time]))
+        np.savetxt(fn ,cm_vals[None],fmt='%d, %d %d %d %f %f')
 
 #tp = cm[0][0], fp = cm[0][1], fn = cm[1][0], tn = cm[1][1]
 #f = open("/Users/mvijayen/bda/processed/k1bs7.txt",'a')
