950eaf53396e825979e7ca7164dcec99be2a2b14
Zhiya Zuo <zhiyazuo@gmail.com>
2015-12-07 20:04:54 -0600

    cast input argument to integers

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from kNNskl import NeonKNN
 
-k = sys.argv[1]
-block_size= sys.argv[2]
+k = int(sys.argv[1])
+block_size= int(sys.argv[2])
 fn = sys.argv[3]
 NeonKNN(k, block_size, fn)
 
