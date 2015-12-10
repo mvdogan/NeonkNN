5e31b5da31729d45efbcd0bfa94f81e3cd9601a2
Joel E Tosado <joel-tosadojimenez@uiowa.edu>
2015-12-09 19:04:52 -0600

    path changes for this thing to work

--- kNNskl.py
+++ kNNskl.py
@@ -12,12 +12,13 @@ def NeonKNN(k=1, block_size=7, logfile='logfile.txt'):
     # Path
     originalTrainPath = "/Shared/bdagroup5/Original/train/"
     skinTrainPath = "/Shared/bdagroup5/Skin/train/"
-
+    resize = 0.1;
+    
     # Load training samples
     imgTrainNames = [f for f in os.listdir(originalTrainPath) if not f.startswith('.')]
-    #imgTrainNames = imgTrainNames[0:199]
+    imgTrainNames = imgTrainNames[0:1]
     train_pi_start = time.time()
-    img_train = pixelArray(originalTrainPath, skinTrainPath, imgTrainNames, blockSize=block_size)
+    img_train = pixelArray(originalTrainPath, skinTrainPath, imgTrainNames, blockSize=block_size,resize)
     train_pi_end = time.time()
     train_pi_time = train_pi_end - train_pi_start
     #train_shape = img_train.shape
@@ -51,14 +52,12 @@ def NeonKNN(k=1, block_size=7, logfile='logfile.txt'):
         for i in imgValNames:
             print 'Working on the {}th images!'.format(counter)
             #print i,
-            img_val = processImage(originalValPath, skinValPath, i, blockSideSize=block_size)
+            img_val = processImageRowTest(originalValPath, skinValPath, i, blockSideSize=block_size,resize)
             X_val = img_val[:,0:-1]
             Y_val = img_val[:,-1]
             knn_predict_start = time.time()
             dist = fitting.kneighbors(X_val,k,return_distance=True)
             del X_val
-            #predictYval = knn.predict(X_val)
-            #upscaleBinary(predictYval,i,0.1)
             knn_predict_end = time.time()
             predict_time = knn_predict_end - knn_predict_start
             #print predict_time,
@@ -81,6 +80,9 @@ def NeonKNN(k=1, block_size=7, logfile='logfile.txt'):
                 sub_cls_array = cls_array[:, :ki]
                 # find the mode in each row
                 _pred = stats.mode(sub_cls_array, axis=1)[0].flatten().tolist()
+                if k==1:
+                    upscaleBinary(_pred,i,resize)
+                    
                 cm = sklearn.metrics.confusion_matrix(Y_val, _pred)        
                 # output the flattened confusion matrix
                 output_row += cm.flatten().tolist()
@@ -92,5 +94,3 @@ def NeonKNN(k=1, block_size=7, logfile='logfile.txt'):
 
             #np.savetxt(fn ,cm_vals[None],fmt='%d, %d %d %d %f %f')
             #print cm_vals
-
-
--- utils.py
+++ utils.py
@@ -89,17 +89,20 @@ def processImage(originalPath, skinPath, imageName, blockSideSize=7, resizeTo = 
     
     return (np.asarray(samples, dtype=np.uint8))
 
-def processImageRowTest(imageName, blockSideSize=7, resizeTo = None, test = 0):
+
+def processImageRowTest(originalPath,skinPath, imageName, blockSideSize=7, resizeTo = 0.1, test = 0):
     '''
-        Update on 12/05/15: Change the resize feature to be a ratio
+        Update on 12\\05\\15: Change the resize feature to be a ratio
     '''
 
     import numpy as np
     import math
     import PIL.Image 
     
-    imagePath  = "../Original/train/"+imageName
-    skinPath = "../Skin/train/"+imageName[0:-4]+"_s.bmp"
+    #imagePath  = "..\\Original\\train\\"+imageName
+    #skinPath = "..\\Skin\\train\\"+imageName[0:-4]+"_s.bmp"
+    imagePath  = originalPath+imageName
+    skinPath = skinPath+imageName[0:-4]+"_s.bmp"
     skin = PIL.Image.open(skinPath)
     image = PIL.Image.open(imagePath)
     imSizeX,imSizeY = image.size
@@ -131,9 +134,9 @@ def processImageRowTest(imageName, blockSideSize=7, resizeTo = None, test = 0):
 
     isSkin = 1
     currentSample=0
-
-    for x in  range(0+fringePixels,imSizeX-fringePixels):
-        for y in range(0+fringePixels,imSizeY-fringePixels):
+    
+    for y in range(0+fringePixels,imSizeY-fringePixels):
+        for x in  range(0+fringePixels,imSizeX-fringePixels):
             if ((np.array(skinPixels[x,y])==255).all()):
                     isSkin=np.uint8(0)
             partialSampleIndex = 0
@@ -169,13 +172,14 @@ def processImageRowTest(imageName, blockSideSize=7, resizeTo = None, test = 0):
     
     return (np.asarray(samples, dtype=np.uint8))
 
-def pixelArray (originalPath, skinPath, imgNames, blockSize=7):
+
+def pixelArray (originalPath, skinPath, imgNames, blockSize=7,resize):
     import numpy as np
     for i,fname in enumerate(imgNames):
         if i==0:
-            pixels = processImage(originalPath, skinPath, fname, blockSideSize=blockSize)
+            pixels = processImageRowTest(originalPath, skinPath, fname, blockSideSize=blockSize,resize)
         else:
-            pixels = np.concatenate((pixels,processImage(originalPath, skinPath, fname, blockSideSize=blockSize)), axis=0)
+            pixels = np.concatenate((pixels,processImageRowTest(originalPath, skinPath, fname, blockSideSize=blockSize,resize)), axis=0)
     return pixels
 
 def originalResize(createResizedImages = 0):
@@ -192,7 +196,7 @@ def originalResize(createResizedImages = 0):
     #create folder structure for where the resized images will reside
     for imagePath in imagePaths:
         for resize in resizeRange:
-            resizedPath = "/Shared/bdagroup5"+str(int(resize*10))+imagePath
+            resizedPath = "/Shared/bdagroup5/"+str(int(resize*10))+imagePath
             if not os.path.exists(resizedPath):
                 os.makedirs(resizedPath)
 
@@ -228,20 +232,85 @@ def originalResize(createResizedImages = 0):
         originalCount = originalCount + 1            
     textFile.close()
 
-
+def getDirectory(imagePaths, toSavePath,resize=0.1,fringe=3):
+    import os
+    import PIL.Image
+        
+    textFile = open("..\\"+toSavePath,"w")
+    for imagePath in imagePaths:
+        imageSubset = os.listdir(""+imagePath)
+        for imageName in imageSubset:   
+            imageOriginal = PIL.Image.open(imagePath+imageName)
+            originalWidth, originalHeight = imageOriginal.size
+            resizedWidth = originalWidth*resize
+            resizedHeight = originalHeight*resize
+            samples = (originalWidth*resize-fringe*2)*(originalHeight*resize-fringe*2)
+            originalSamples = (originalWidth-fringe*2)*(originalHeight*fringe*2)            
+            textFile.write("{0},{1},{2},{3},{4},{5},{6}\n".format(imageName,originalWidth,originalHeight,originalSamples,int(resizedWidth),int(resizedHeight),int(samples)))
+    textFile.close()
+    
 def upscaleBinary(oneDArray,imageName,resize):
     import PIL.Image
     import math
+    import numpy as np
 
+    # can be put in parameter but
+    fringe = 3 #6 for 7x7
+    
+    oneDArray = (oneDArray*255).tolist()
+    
+    print "Starting upscale"
     originalResize()
     origImageSizes = open("/Shared/bdagroup5/OriginalImageSizes.txt","r")
     data = origImageSizes.readlines()
     collectImageDimensions = []
     for eachImage in data:
         collectImageDimensions.append(eachImage[:-1].split('\t'))
+    origImageSizes.close()
 
     original = collectImageDimensions[np.where(np.array(collectImageDimensions) == imageName)[0][0]]
-    binaryPredicted = PIL.Image.new('L', (math.floor(int(original[1])*resize),math.floor(int(original[2])*resize))) 
+    print original
+    resizedWidthWithFringe = int(math.floor(int(original[1])*resize))-fringe*2
+    resizedHeightWithFringe = int(math.floor(int(original[2])*resize))-fringe*2
+    print resizedWidthWithFringe,resizedHeightWithFringe
+
+##  All ones binary   
+##  oneDArray = np.ones((resizedWidthWithFringe * resizedHeightWithFringe))*255
+
+    print str(resizedWidthWithFringe * resizedHeightWithFringe)
+    print len(oneDArray)
+
+##  STRIPING
+##    for lineIndex in range(0,resizedWidthWithFringe * resizedHeightWithFringe,resizedWidthWithFringe):
+##        if lineIndex%(resizedWidthWithFringe*2)==0:
+##            oneDArray[lineIndex:lineIndex+resizedWidthWithFringe]=0
+            
+    binaryPredicted = PIL.Image.new('L', (resizedWidthWithFringe,resizedHeightWithFringe))
+    print binaryPredicted.size
     binaryPredicted.putdata(oneDArray)
-    originalImage = binaryPredicted.resize(int(original[1]),int(original[2]),PIL.Image.ANTIALIAS)
-    bw.save("/Shared/bdagroup5/greyscaleImages/predicted_"+imageName)
+    binaryPredicted.save("/Shared/bdagroup5/greyscaleImages/BEFORE_RESIZE_"+imageName[0:-4]+".png","PNG")    
+    originalImageGrey = binaryPredicted.resize((int(original[1])-int(fringe*2/resize),int(original[2])-int(fringe*2/resize)),PIL.Image.ANTIALIAS)
+    originalImageGrey = originalImageGrey.point(lambda x: 0 if x<128 else 255, '1')
+    originalImageGrey.save("/Shared/bdagroup5/greyscaleImages/predicted_resizeFrom"+str(int(resize*10))+"_"+imageName[0:-4]+".png","PNG")    
+    greyscaleData = originalImageGrey.load()
+    
+##    originalRGBImage = PIL.Image.open("..\\Original\\train\\"+imageName)
+    originalRGBImage = PIL.Image.open("/Shared/bdagroup5/Original/val/"+imageName)
+    dataOriginalRGB = originalRGBImage.load()
+    
+    changedToRGB = np.ones((int(original[2]),int(original[1]),3),dtype = np.uint8)
+    changedToRGB[:,:] = [255,255,255]
+    print changedToRGB.shape
+  
+    for y in range(int(fringe/resize),int(original[2])-int(fringe/resize)):
+        for x in range(int(fringe/resize),int(original[1])-int(fringe/resize)):
+            if greyscaleData[x-int(fringe/resize),y-int(fringe/resize)]==255: #if predicted skin
+                changedToRGB[y,x] = np.array(dataOriginalRGB[x,y]).tolist()
+
+    print changedToRGB.shape
+    originalRGBImage =  PIL.Image.fromarray(changedToRGB,'RGB')
+    originalRGBImage.save("/Shared/bdagroup5/greyscaleImages/predictedRGB_resizeFrom"+str(int(resize*10))+"_"+imageName[0:-4]+".png","PNG")
+    
+    originalRGBImage.close()
+    originalImageGrey.close()
+    binaryPredicted.close()
