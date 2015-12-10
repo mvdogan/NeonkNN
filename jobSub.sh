ae0bfaa420e76b47fde6db26ef11e485b39388b3
Joel Tosado Jimenez <tosadojimenez@neon-login-0-1.local>
2015-12-09 20:25:06 -0600

    changed job sub file to myself cuz selfish?

--- jobSub.sh
+++ jobSub.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-module load python
 ## Name of job
-#$-N test-1-9-1
+#$-N Santa-1-9-1
 ## Concatenate SGE output? (recommend yes)
 #$-j Y
 ## Parallel environment
-#$-pe smp 1
+#$-pe smp 4
 ## Next choose a queue
-#$-q INFORMATICS
+#$-q COE
 ## Set the working directory to current directory
 #$-cwd
 ## Set email address
-#$-M zhiya-zuo@uiowa.edu
+#$-M joel-tosadojimenez@uiowa.edu
 ## Send me email at beginning, end, abort, suspend
 #$-m beas
 ## Put your command or other application last
-python main.py 1 9 neon_1_9.csv
+/Users/tosadojimenez/anaconda2/bin/python main.py 1 7 santa_1_9.csv
