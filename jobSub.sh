a00baf4bdc594b65bbc1351b2dafb70bff39b285
Meeshanthini Dogan <mvdogan@users.noreply.github.com>
2015-12-12 19:53:46 -0600

    Update jobSub.sh

--- jobSub.sh
+++ jobSub.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ## Name of job
-#$-N Santa-1-9-1
+#$-N skinDetection
 ## Concatenate SGE output? (recommend yes)
 #$-j Y
 ## Parallel environment
-#$-pe smp 4
+#$-pe 24cpn 24
 ## Next choose a queue
-#$-q COE
+#$-q UI-HM
 ## Set the working directory to current directory
 #$-cwd
 ## Set email address
@@ -14,4 +14,4 @@
 ## Send me email at beginning, end, abort, suspend
 #$-m beas
 ## Put your command or other application last
-/Users/tosadojimenez/anaconda2/bin/python main.py 1 7 santa_1_9.csv
+/Users/tosadojimenez/anaconda2/bin/python main.py 9 5
