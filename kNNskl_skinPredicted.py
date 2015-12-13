b37ff5e323fac282fa1cac08e53cf147999fe5a2
Zhiya Zuo <zhiyazuo@gmail.com>
2015-12-13 10:23:53 -0600

    Remove the part writing to knnprocessed

--- kNNskl_skinPredicted.py
+++ kNNskl_skinPredicted.py
@@ -49,49 +49,46 @@ def NeonKNN_test(k=1, block_size=7, testIndexFrom=0,testIndexTo=2000, logfile='l
     #print imgValNames
 
     counter = 1
-    with open("/Shared/bdagroup5/kNNprocessed/{}".format(logfile),'w') as fn:
-        # writer for this filename fn
-        w = csv.writer(fn)
-        for i in imgValNames:
-            print 'Working on the {}th images!'.format(counter)
-            #print i,
-            img_val = processImageRowTest(originalValPath, skinValPath, i, blockSideSize=block_size,resizeTo=resize,test=1)
-            #X_val = img_val[:,0:-1]
-            X_val = img_val[:,:]
-            #Y_val = img_val[:,-1]
-            knn_predict_start = time.time()
-            dist = fitting.kneighbors(X_val,k,return_distance=True)
-            del X_val
-            knn_predict_end = time.time()
-            predict_time = knn_predict_end - knn_predict_start
-            #print predict_time,
-            knn_time = fit_time + predict_time
-            #percent_accuracy = sklearn.metrics.accuracy_score(Y_val, predictYval, normalize=True, sample_weight=None)*100
-            #cm = sklearn.metrics.confusion_matrix(Y_val, predictYval)        
-            #cm_vals = np.concatenate(([cm[0][0]], [cm[0][1]], [cm[1][0]], [cm[1][1]], [percent_accuracy], [knn_time]))
+    for i in imgValNames:
+        print 'Working on the {}th images!'.format(counter)
+        #print i,
+        img_val = processImageRowTest(originalValPath, skinValPath, i, blockSideSize=block_size,resizeTo=resize,test=1)
+        #X_val = img_val[:,0:-1]
+        X_val = img_val[:,:]
+        #Y_val = img_val[:,-1]
+        knn_predict_start = time.time()
+        dist = fitting.kneighbors(X_val,k,return_distance=True)
+        del X_val
+        knn_predict_end = time.time()
+        predict_time = knn_predict_end - knn_predict_start
+        #print predict_time,
+        knn_time = fit_time + predict_time
+        #percent_accuracy = sklearn.metrics.accuracy_score(Y_val, predictYval, normalize=True, sample_weight=None)*100
+        #cm = sklearn.metrics.confusion_matrix(Y_val, predictYval)        
+        #cm_vals = np.concatenate(([cm[0][0]], [cm[0][1]], [cm[1][0]], [cm[1][1]], [percent_accuracy], [knn_time]))
 
-            # Use csv.writerow()
-            #w.writerow([i] + cm.flatten().tolist() + [percent_accuracy, knn_time])
+        # Use csv.writerow()
+        #w.writerow([i] + cm.flatten().tolist() + [percent_accuracy, knn_time])
 
-            # Match the indices to their corresponding classes
-            dist_array, ind_array = dist[0], dist[1]
-            cls_array = _match_class(Y_train, ind_array)
+        # Match the indices to their corresponding classes
+        dist_array, ind_array = dist[0], dist[1]
+        cls_array = _match_class(Y_train, ind_array)
 
-            output_row = [i, knn_time] 
+        output_row = [i, knn_time] 
 
-            # for each k, get confusion matrix
-            sub_cls_array = cls_array[:]
-            # find the mode in each row
-            _pred = stats.mode(sub_cls_array, axis=1)[0].flatten().tolist()
-            upscaleBinary(_pred,i,resize,int(block_size/2))
-            #cm = sklearn.metrics.confusion_matrix(Y_val, _pred)        
-            # output the flattened confusion matrix
-            #output_row += cm.flatten().tolist()
+        # for each k, get confusion matrix
+        sub_cls_array = cls_array[:]
+        # find the mode in each row
+        _pred = stats.mode(sub_cls_array, axis=1)[0].flatten().tolist()
+        upscaleBinary(_pred,i,resize,int(block_size/2))
+        #cm = sklearn.metrics.confusion_matrix(Y_val, _pred)        
+        # output the flattened confusion matrix
+        #output_row += cm.flatten().tolist()
 
-            # headers: Image name; Confusion entries for all k's; train tim
-            #w.writerow(output_row)
+        # headers: Image name; Confusion entries for all k's; train tim
+        #w.writerow(output_row)
 
-            counter += 1
+        counter += 1
 
-            #np.savetxt(fn ,cm_vals[None],fmt='%d, %d %d %d %f %f')
-            #print cm_vals
+        #np.savetxt(fn ,cm_vals[None],fmt='%d, %d %d %d %f %f')
+        #print cm_vals
